95b546d9d5c094dbca21d0dbe7e7c6ec507c564f
gabrielrodrigues <gabriel.grf8395@gmail.com>
2023-04-08 13:53:37 -0300

    função de adicionar tarefa

--- web/src/screens/Home/index.tsx
+++ web/src/screens/Home/index.tsx
@@ -1,8 +1,47 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+import Swal from 'sweetalert2';
+
 import Logo from '../../assets/images/logo.png';
 import Plus from '../../assets/svg/plus.svg';
+import { TaskDTO } from '../../dtos/TaskDTO';
 import styles from './styles.module.css';
 
 export function Home() {
+  const [taskDescription, setTaskDescription] = useState<string>('');
+  const [tasks, setTasks] = useState<TaskDTO[]>([]);
+
+  class Form {
+    handleNewTaskChange(e: ChangeEvent<HTMLInputElement>) {
+      setTaskDescription(e.target.value);
+    }
+  }
+
+  function handleAddTask(e: FormEvent) {
+    e.preventDefault();
+    var existingTask = false;
+
+    tasks.filter((item) => {
+      if (item.taskDescription === taskDescription) {
+        existingTask = true;
+      }
+    });
+
+    if (existingTask) {
+      Swal.fire({
+        title: 'Problemas ao adicionar tarefa',
+        text: 'Esta tarefa já existe! Crie uma com um nome diferente',
+        timer: 3000,
+        timerProgressBar: true,
+        background: 'var(--gray-500)',
+        color: 'var(--gray-100)',
+      });
+    } else {
+      setTasks((prevState) => [...prevState, { taskDescription, finishedTask: false }]);
+      setTaskDescription('');
+    }
+  }
+
   return (
     <div className={styles.container}>
       <header className={styles.header_box}>
@@ -10,8 +49,14 @@ export function Home() {
       </header>
 
       <main className={styles.content_box}>
-        <form className={styles.form_add_tasks}>
-          <input className={styles.tasks_input} placeholder="Adicione uma nova tarefa" type="text" />
+        <form className={styles.form_add_tasks} onSubmit={handleAddTask}>
+          <input
+            className={styles.tasks_input}
+            onChange={new Form().handleNewTaskChange}
+            placeholder="Adicione uma nova tarefa"
+            type="text"
+            value={taskDescription}
+          />
           <button>
             Criar
             <img className={styles.plus_icon} src={Plus} />
